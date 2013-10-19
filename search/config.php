044b3fa99c11508d2f1c18679ea3c458b32df815
WINNERAS <winneras@gmail.com>
2013-10-20 00:38:55 +0100

    完成用户表，准备开始抓取用户信息

--- search/config.php
+++ search/config.php
@@ -9,10 +9,11 @@ $twitterSettings = array(
     'consumer_secret' => ""
 );
 $dbSettings = array(
-'host' => 'localhost',
- 'port' => '8889',
- 'user' => 'root',
- 'pass' => 'root'
+    'host' => '127.0.0.1',
+    'port' => '8889',
+    'user' => 'root',
+    'pass' => 'root',
+    'dbName' => 'ctwcount'
 );
 
 $con = mysql_connect("$dbSettings[host]:$dbSettings[port]", "$dbSettings[user]", "$dbSettings[pass]");
@@ -20,4 +21,18 @@ $con = mysql_connect("$dbSettings[host]:$dbSettings[port]", "$dbSettings[user]",
 if (!$con) {
     die('Can not connect:' . mysql_errno());
 }
+
+$dbSelected = mysql_select_db("$dbSettings[dbName]", $con);
+if (!$dbSelected) {
+    echo 'test3';
+    die("Can\'t use test_db : " . mysql_error());
+}
+mysql_query("SET NAMES 'utf8'");
+
+/*$q = "INSERT INTO `zh_tw_info` (`UID`, `User_Screen_Name`, `User_Name`, `User_Location`, `User_Description`, `User_Followers`, `User_Friends`, `User_Created_At`, `User_Time_Zone`, `User_Language`, `User_Profile_Image`, `User_Profile_Image_Https`, `User_Appear_Counter`, `User_Last_Appear`) VALUES
+(2567531, 'gslin', 'Gea-Suan Lin', 'Taipei, Taiwan', '??事是?步最大的原?力', 1624, 338, 'Tue Mar 27 22:25:29 +0000 2007', 'Taipei', 'en', 'http://a0.twimg.com/profile_images/378800000483574455/15c3e60288d463018de0214ffd20e36d_normal.jpeg', 'https://si0.twimg.com/profile_images/378800000483574455/15c3e60288d463018de0214ffd20e36d_normal.jpeg', 1, '2013-10-19 22:09:39'),
+(6842422, 'Thruth', 'Thruth Wang', 'U.S.A', 'I have no money, no resources, no hopes. I am the happiest man alive.', 6767, 432, 'Fri Jun 15 21:32:38 +0000 2007', 'Beijing', 'en', 'http://a0.twimg.com/profile_images/1470925253/6wfvx_normal.jpeg', 'https://si0.twimg.com/profile_images/1470925253/6wfvx_normal.jpeg', 1, '2013-10-19 22:10:42')
+ON DUPLICATE KEY UPDATE  User_Appear_Counter = User_Appear_Counter +1";
+$r = mysql_query($q);
+print_r($r);*/
 ?>
--- search/index.php
+++ search/index.php
@@ -5,8 +5,8 @@ require_once('config.php');
 $url = 'https://api.twitter.com/1.1/search/tweets.json';
 //$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
 $requestMethod = 'GET';
-
-$getfield = '?count=3&lang=zh&q=%E7%9A%84+OR+%E4%BA%86+OR+%E6%98%AF+OR+%E4%B8%8D+OR+%E5%9C%A8+OR+%E6%9C%89+OR+%E6%88%91+OR+%E4%BB%96+OR+%E5%B0%B1+OR+%E4%B9%9F+OR+%E4%BD%A0+OR+%E5%88%B0+OR+%E6%9D%A5+OR+%E8%BF%98+OR+%E6%8A%8A+OR+%E5%8E%BB+OR+%E9%82%A3&result_type=recent';
+$getcount = 3;
+$getfield = '?count='.$getcount.'&lang=zh&q=%E7%9A%84+OR+%E4%BA%86+OR+%E6%98%AF+OR+%E4%B8%8D+OR+%E5%9C%A8+OR+%E6%9C%89+OR+%E6%88%91+OR+%E4%BB%96+OR+%E5%B0%B1+OR+%E4%B9%9F+OR+%E4%BD%A0+OR+%E5%88%B0+OR+%E6%9D%A5+OR+%E8%BF%98+OR+%E6%8A%8A+OR+%E5%8E%BB+OR+%E9%82%A3&result_type=recent';
 
 $twitter = new TwitterAPIExchange($twitterSettings);
 $response = $twitter->setGetfield($getfield)
@@ -14,59 +14,87 @@ $response = $twitter->setGetfield($getfield)
         ->performRequest();
 $res_array = json_decode($response);
 //var_dump($res_array);
-$counter = 1;
+$counter = 0;
+$query_string = "INSERT INTO `ctwcount`.`zh_tw_info` 
+        (`UID`, `User_Screen_Name`, `User_Name`, `User_Location`, `User_Description`, `User_Followers`, `User_Friends`, 
+         `User_Created_At`, `User_Time_Zone`, `User_Language`, `User_Profile_Image`, `User_Profile_Image_Https`, `User_Appear_Counter`, `User_Last_Appear`) 
+        VALUES";
 
-foreach ($res_array -> statuses as $tweet) {
-    echo 'Tweet No.'.$counter;
+foreach ($res_array->statuses as $tweet) {
     $counter++;
+ /*   echo '--------';
+    echo 'Tweet No.' . $counter;
+    echo '--------'; 
     echo '<br>';
-    echo 'Tweet at '.$tweet -> created_at;
+    echo 'Tweet at ' . $tweet->created_at;
     echo '<br>';
-    echo 'Tweet ID '.$tweet -> id;
+    echo 'Tweet ID ' . $tweet->id;
     echo '<br>';
-    echo 'Tweet text: '. $tweet -> text;
+    echo 'Tweet text: ' . $tweet->text;
     echo '<br>';
-    echo 'Tweet source: '. $tweet -> source;
+    echo 'Tweet source: ' . $tweet->source;
     echo '<br>';
-    echo 'Reply to tweet ID: '. $tweet -> in_reply_to_status_id;
+    echo 'Reply to tweet ID: ' . $tweet->in_reply_to_status_id;
     echo '<br>';
-    echo 'Reply to user ID: '. $tweet -> in_reply_to_user_id;
+    echo 'Reply to user ID: ' . $tweet->in_reply_to_user_id;
     echo '<br>';
-    echo 'Reply to user screen name: '.$tweet -> in_reply_to_screen_name;
+    echo 'Reply to user screen name: ' . $tweet->in_reply_to_screen_name;
     echo '<br>';
-    echo 'Language: '. $tweet -> lang;
+    echo 'Language: ' . $tweet->lang;
     echo '<br>';
     echo '<br>';
     echo '<br>';
-    echo 'User ID:'. $tweet -> user -> id;
+    echo 'User ID:' . $tweet->user->id;
     echo '<br>';
-    echo 'User name: ' . $tweet -> user -> name;
+    echo 'User name: ' . $tweet->user->name;
     echo '<br>';
-    echo 'User screen name: ' . $tweet -> user -> screen_name;
+    echo 'User screen name: ' . $tweet->user->screen_name;
     echo '<br>';
-    echo 'User Location: ' . $tweet -> user -> location;
+    echo 'User Location: ' . $tweet->user->location;
     echo '<br>';
-    echo 'User description: ' . $tweet -> user -> description;
+    echo 'User description: ' . $tweet->user->description;
     echo '<br>';
-    echo 'User followers count: '. $tweet -> user -> followers_count;
+    echo 'User followers count: ' . $tweet->user->followers_count;
     echo '<br>';
-    echo 'User friends count: '. $tweet -> user -> friends_count;
+    echo 'User friends count: ' . $tweet->user->friends_count;
     echo '<br>';
-    echo 'User created at '. $tweet -> user -> created_at;
+    echo 'User created at ' . $tweet->user->created_at;
     echo '<br>';
-    echo 'User time zone: '. $tweet -> user -> time_zone;
+    echo 'User time zone: ' . $tweet->user->time_zone;
     echo '<br>';
-    echo 'User language: ' . $tweet -> user -> lang;
+    echo 'User language: ' . $tweet->user->lang;
     echo '<br>';
-    echo 'User profile image: ' . $tweet -> user -> profile_image_url;
+    echo 'User profile image: ' . $tweet->user->profile_image_url;
     echo '<br>';
-    echo 'User profile image https:' . $tweet -> user -> profile_image_url_https;
+    echo 'User profile image https:' . $tweet->user->profile_image_url_https;
     echo '<br>';
-    
-    
+
+
     //var_dump($tweet);
-    echo '<br>';
-    echo '-------------------------------------';
-    echo '<br>';
+    echo '<br>';*/
+
+    //database string
+    /* $query_string = "INSERT INTO `ctwcount`.`zh_tw_info` 
+      (`UID`, `User_Screen_Name`, `User_Name`, `User_Location`, `User_Description`, `User_Followers`, `User_Friends`,
+      `User_Created_At`, `User_Time_Zone`, `User_Language`, `User_Profile_Image`, `User_Profile_Image_Https`, `User_Appear_Counter`, `User_Last_Appear`)
+      VALUES
+      ('".$tweet -> user -> id."', '".$tweet -> user -> screen_name."', '".$tweet -> user -> name."',
+      '".$tweet -> user -> location."', '".$tweet -> user -> description."', '".$tweet -> user -> followers_count."',
+      '".$tweet -> user -> friends_count."', '".$tweet -> user -> created_at."', '".$tweet -> user -> time_zone."',
+      '".$tweet -> user -> lang."', '".$tweet -> user -> profile_image_url."', '".$tweet -> user -> profile_image_url_https."', '1', CURRENT_TIMESTAMP)
+      ON DUPLICATE KEY UPDATE  User_Appear_Counter = User_Appear_Counter +1"; */
+    $query_string .= "('" . $tweet->user->id . "', '" . $tweet->user->screen_name . "', '" . $tweet->user->name . "', 
+         '" . $tweet->user->location . "', '" . $tweet->user->description . "', '" . $tweet->user->followers_count . "', 
+         '" . $tweet->user->friends_count . "', '" . $tweet->user->created_at . "', '" . $tweet->user->time_zone . "', 
+         '" . $tweet->user->lang . "', '" . $tweet->user->profile_image_url . "', '" . $tweet->user->profile_image_url_https . "', '1', CURRENT_TIMESTAMP)";
+    if($counter < $getcount){ $query_string .= ',';}
+
 }
+
+
+$query_string .="ON DUPLICATE KEY UPDATE  User_Appear_Counter = User_Appear_Counter +1" ;
+echo $query_string;
+$result = mysql_query($query_string);
+echo '<br>';
+print_r($result);
 ?>
